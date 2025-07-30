f7267be2ebc54d7acff7a023694f0ce78b5d0674
Naufal Haidir Ridha <naufalhaidirridha@rocketmail.com>
2025-07-31 01:02:47 +0700

    1

---
+++ resources/views/layouts/backend/__includes/sidebar-item.blade.php
@@ -0,0 +1,28 @@
+<li class="menu-section">
+  <h4 class="menu-text"> Main </h4>
+  <i class="menu-icon ki ki-bold-more-hor icon-md"></i>
+</li>
+<li class="menu-item menu-item-submenu {{ (request()->is('dashboard/profiles*')) ? 'menu-item-open' : '' }}">
+  <a class="menu-link menu-toggle">
+    <i class="menu-icon fas fa-users"></i>
+    <span class="menu-text"> Broadcasters </span>
+    <i class="menu-arrow"></i>
+  </a>
+  <div class="menu-submenu">
+    <ul class="menu-subnav">
+      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/broadcasters/members*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/broadcasters/members') }}" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> Members </span></a></li>
+    </ul>
+  </div>
+</li>
+<li class="menu-item menu-item-submenu {{ (request()->is('dashboard/profiles*')) ? 'menu-item-open' : '' }}">
+  <a class="menu-link menu-toggle">
+    <i class="menu-icon fas fa-users"></i>
+    <span class="menu-text"> Families </span>
+    <i class="menu-arrow"></i>
+  </a>
+  <div class="menu-submenu">
+    <ul class="menu-subnav">
+      <li class="menu-item menu-item-submenu {{ (request()->is('dashboard/families/members*')) ? 'menu-item-active' : '' }}"><a href="{{ url('/dashboard/families/members') }}" class="menu-link"><span class="menu-text"><i class="menu-bullet menu-bullet-dot"><span></span></i> Members </span></a></li>
+    </ul>
+  </div>
+</li>
